a7159e7524af0c62e97c4b1cd4dbff46d5a1598f
Mikhail Gorbachev <gor.mihal@gmail.com>
2019-12-04 11:26:24 +0300

    Add student to mock data

--- dist/main-es5.js
+++ dist/main-es5.js
@@ -579,8 +579,13 @@
                 { id: 1, firstName: "Mikhail", lastName: "Gorbachev", middleName: "Aleksandrowich", birth: new Date("1998-12-10"), score: 4 },
                 { id: 2, firstName: "Oleg", lastName: "Ivanov", middleName: "MIkhailowich", birth: new Date("1995-05-10"), score: 2 },
                 { id: 3, firstName: "Vadim", lastName: "Veselov", middleName: "Sergeevich", birth: new Date("1995-05-10"), score: 1 },
-                { id: 4, firstName: "Oleg", lastName: "Veselov", middleName: "Sergeevich", birth: new Date("1995-05-10"), score: 1 },
-                { id: 6, firstName: "Ashot", lastName: "Veselov", middleName: "Sergeevich", birth: new Date("1995-05-10"), score: 1 },
+                { id: 4, firstName: "Oleg", lastName: "Veselov", middleName: "Sergeevich", birth: new Date("1985-08-12"), score: 1 },
+                { id: 5, firstName: "Ashot", lastName: "Veselov", middleName: "Sergeevich", birth: new Date("1994-05-10"), score: 3 },
+                { id: 6, firstName: "Evgenii", lastName: "Abramov", middleName: "Sergeevich", birth: new Date("1995-05-10"), score: 4 },
+                { id: 7, firstName: "Vasilii", lastName: "Kyrenkov", middleName: "Sergeevich", birth: new Date("1998-12-10"), score: 5 },
+                { id: 8, firstName: "Artem", lastName: "Molodec", middleName: "Sergeevich", birth: new Date("1993-01-12"), score: 2 },
+                { id: 9, firstName: "Nastya", lastName: "Safronova", middleName: "Sergeevich", birth: new Date("2000-07-12"), score: 3 },
+                { id: 10, firstName: "Igor", lastName: "Anisimov", middleName: "Sergeevich", birth: new Date("2002-01-12"), score: 5 },
             ];
             /***/ 
         }),
