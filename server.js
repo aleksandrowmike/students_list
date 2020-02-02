ca64aa60a96f0c402dda2e6bcf0a23cc13e2a958
Mikhail Gorbachev <gor.mihal@gmail.com>
2020-02-02 14:45:34 +0300

    Add compression

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const path = require('path');
+const compression = require('compression');
 const app = express();
+
+app.use(compression({
+  threshold: 1,
+  filter: function() {return true;}
+}));
 
 // Serve static files....
 app.use(express.static(__dirname + '/dist/students'));
